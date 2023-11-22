b930131592abb801e617a1aab92c86a24d0a22b2
justin-h-mills <80857684+justin-h-mills@users.noreply.github.com>
2023-11-22 01:03:45 +0000

    implemented html2markdown.py solution

--- challenge/html2markdown.py
+++ challenge/html2markdown.py
@@ -1,4 +1,18 @@
+import re
 
-def html2markdown(html):
+def html2markdown(html: str):
     '''Take in html text as input and return markdown'''
-    pass
+    
+    # replace italics
+    markdown = re.sub(r'<em>(.*?)</em>', r'*\1*', html)
+
+    # replace consecutive spaces or line breaks with a single space
+    markdown = re.sub(r'\s+', ' ', markdown)
+
+    # replace paragraphs with 2 line breaks
+    markdown = re.sub(r'<p>(.*?)</p>', r'\1\n\n', markdown)
+
+    # convert html links to markdown format
+    markdown = re.sub(r'<a\s+href=[\'"](.*?)[\'"].*?>(.*?)</a>', r'[\2](\1)', markdown)
+
+    return markdown.strip()
