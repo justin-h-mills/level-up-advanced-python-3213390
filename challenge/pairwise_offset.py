043eac61c15dbe3e900098f91b8a572c08497f21
justin-h-mills <80857684+justin-h-mills@users.noreply.github.com>
2023-11-22 22:24:56 +0000

    Implemented pairwise_offset solution

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,7 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence: list[any], fillvalue: str = '*', offset: int = 0) -> list[tuple[any, any]]:
+    # Create lists for padding with the specified fill value and concatenate with the original sequence
+    top = list(sequence) + [fillvalue] * offset
+    bottom = [fillvalue] * offset + list(sequence)
+
+    # Pair the elements of the top and bottom lists and return as a list of tuples
+    return list(zip(top, bottom))
